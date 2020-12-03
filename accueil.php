da0b269eac1c2b0b11ab442c2a3d937ecfc16df5
fchauv <fchauveau@et.intechinfo.fr>
2020-12-03 13:10:33 +0100

    debut

---
+++ accueil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>accueil</title>
+        <meta charset=utf-8>
+        <link rel="stylesheet" href="entraide.css">
+    <head>
+    <body>
+
+    </body>
+</html>
